bc7911e297a38517e65c9dd86d536f22a7613ceb
-Denux <timon.klinkert@gmail.com>
2022-08-04 00:15:34 +0200

    added some java docs

--- src/main/java/dev/denux/jdaoauth2/JDOAuth2Builder.java
+++ src/main/java/dev/denux/jdaoauth2/JDOAuth2Builder.java
@@ -16,31 +16,60 @@ public class JDOAuth2Builder {
         config = new JDOAuth2Config();
     }
 
+    /**
+     * Sets the client id.
+     * @param clientId the client id.
+     * @return the builder. Good for chaining.
+     */
     public @Nonnull JDOAuth2Builder setClientId(@Nonnull String clientId) {
         config.setClientId(clientId);
         return this;
     }
 
+    /**
+     * Sets the client secret.
+     * @param clientSecret the client secret.
+     * @return the builder. Good for chaining.
+     */
     public @Nonnull JDOAuth2Builder setClientSecret(@Nonnull String clientSecret) {
         config.setClientSecret(clientSecret);
         return this;
     }
 
+    /**
+     * Sets the redirect uri.
+     * @param redirectUri the redirect uri.
+     * @return the builder. Good for chaining.
+     */
     public @Nonnull JDOAuth2Builder setRedirectUri(@Nonnull String redirectUri) {
         config.setRedirectUri(redirectUri);
         return this;
     }
 
+    /**
+     * Sets the scopes.
+     * @param scopes the scopes.
+     * @return the builder. Good for chaining.
+     */
     public @Nonnull JDOAuth2Builder setScopes(@Nonnull Scope... scopes) {
         config.setScopes(Arrays.stream(scopes).collect(Collectors.toSet()));
         return this;
     }
 
+    /**
+     * Sets a custom http client.
+     * @param httpClient the http client.
+     * @return the builder. Good for chaining.
+     */
     public @Nonnull JDOAuth2Config setHttpClient(@Nonnull OkHttpClient httpClient) {
         config.setHttpClient(httpClient);
         return config;
     }
 
+    /**
+     * Checks everything and returns the finished {@link JDOAuth2} instance.
+     * @return the finished {@link JDOAuth2} instance.
+     */
     public @Nonnull JDOAuth2 build() {
         if (config.getClientId() == null) {
             throw new IllegalStateException("Client ID is not set");
